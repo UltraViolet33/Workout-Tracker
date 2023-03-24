00cf02e5b1aeb89eb26237df3b9a1590f3a094b7
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-03-24 10:49:29 +0100

    display all categories

--- app/categories.py
+++ app/categories.py
@@ -20,11 +20,25 @@ def create_category():
     form = CategoryForm()
 
     if form.validate_on_submit():
-        category = Category(name=form.name.data)
+        check_category_exits = Category.query.filter_by(name=form.name.data).first()
+        print(check_category_exits)
 
-        db.session.add(category)
-        db.session.commit()
-        flash("Category created !")
-        return redirect("/all")
+        if check_category_exits == None:
+
+            category = Category(name=form.name.data)
+            db.session.add(category)
+            db.session.commit()
+            flash("Category created !")
+            return redirect("/categories/all")
+
+        else:
+            flash("Category name alreadry exist !", category="error")
 
     return render_template("formCategory.html", form=form)
+
+
+@categories.route("/all", methods=["GET"])
+def get_all_categories():
+
+    categories = Category.query.all()
+    return render_template("categories.html", categories=categories)
