61f1fe9d8bd79e86d7b307806ac4505221a66add
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-03-25 11:53:35 +0100

    add session Exercises model

---
+++ app/models/Session.py
@@ -0,0 +1,32 @@
+from .. import db
+from sqlalchemy.sql import func
+from datetime import datetime
+
+
+class Session_Exercises(db.Model):
+    __tablename__ = "session_exercises"
+
+    session_id = db.Column(db.Integer, db.ForeignKey("sessions.id"),primary_key=True)
+    exercise_id = db.Column(db.Integer, db.ForeignKey("exercises.id"), primary_key=True)
+    series = db.Column("series", db.Integer)
+
+
+    session = db.relationship("Session", back_populates="exercises")
+    # exercise = db.relationship("Exercise", back_populates="exercises")
+
+
+
+
+class Session(db.Model):
+    __tablename__ = 'sessions'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.DateTime(
+        timezone=True), default=func.now(), nullable=False)
+    
+    # session_exercises = db.relationship("Exercise", secondary=sessions_list_exercises, backref="exercises")
+
+    exercises = db.relationship("Session_Exercises", back_populates="session")
+
+    def __init__(self):
+        self.date = datetime.now()
